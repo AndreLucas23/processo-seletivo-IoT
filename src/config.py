5f567acffec6e471176da77b042f9b9515b6a37f
André Lucas <sl.andrelucas@gmail.com>
2026-05-02 14:56:20 -0300

    refactor: add config.py and change constants

---
+++ src/config.py
@@ -0,0 +1,19 @@
+FREQUENCY = 1000
+
+class Pins:
+    I2C_SDA = 21
+    I2C_SCL = 22
+    LED_RED = 25
+    LED_GREEN = 26
+    LED_BLUE = 27
+
+class Thresholds:
+    ACCEL_DANGER = 2
+    ACCEL_DANGER_SINGLE = 1.48
+    ACCEL_CRITICAL = 2.8
+    ACCEL_CRITICAL_SINGLE = 1.84
+    GYRO_DANGER = 150
+
+class Sensitivities:
+    ACCEL = 16384
+    GYRO = 131
--- src/main.py
+++ src/main.py
@@ -4,36 +4,27 @@ from math import sqrt
 
 from mpu6050 import accel
 from ssd1306 import SSD1306_I2C
+from config import *
 
-def set_led_color(led_percent_red, led_percent_green, led_percent_blue):
-    led_red_duty = int(led_percent_red / 100 * 1023)
-    led_green_duty = int(led_percent_green / 100 * 1023)
-    led_blue_duty = int(led_percent_blue / 100 * 1023)
+def set_led_color(led_red_percent, led_green_percent, led_blue_percent):
+    led_red_duty = int(led_red_percent / 100 * 1023)
+    led_green_duty = int(led_green_percent / 100 * 1023)
+    led_blue_duty = int(led_blue_percent / 100 * 1023)
 
     led_red.duty(led_red_duty)
     led_green.duty(led_green_duty)
     led_blue.duty(led_blue_duty)
 
-FREQUENCY = 1000
-
-i2c = SoftI2C(sda=Pin(21), scl=Pin(22))
+i2c = SoftI2C(sda=Pin(Pins.I2C_SDA), scl=Pin(Pins.I2C_SCL))
 
 mpu = accel(i2c)
 
 oled = SSD1306_I2C(128, 64, i2c)
 oled.fill(0)
 
-led_red = PWM(Pin(25), freq=FREQUENCY)
-led_green = PWM(Pin(26), freq=FREQUENCY)
-led_blue = PWM(Pin(27), freq=FREQUENCY)
-
-ACCEL_DANGER_THRESHOLD = 2
-ACCEL_SINGLE_DANGER_THRESHOLD = 1.48
-ACCEL_CRITICAL_THRESHOLD = 2.8
-ACCEL_SINGLE_THRESHOLD = 1.84
-GYRO_DANGER_THRESHOLD = 150
-ACCEL_SENSITIVITY = 16384
-GYRO_SENSITIVITY = 131
+led_red = PWM(Pin(Pins.LED_RED), freq=FREQUENCY)
+led_green = PWM(Pin(Pins.LED_GREEN), freq=FREQUENCY)
+led_blue = PWM(Pin(Pins.LED_BLUE), freq=FREQUENCY)
 
 print('Teste')
 print('Sensor MPU6050 iniciado')
@@ -45,13 +36,13 @@ while True:
     try:
         mpu_values = mpu.get_values()
 
-        mpu_accel_x = mpu_values['AcX'] / ACCEL_SENSITIVITY
-        mpu_accel_y = mpu_values['AcY'] / ACCEL_SENSITIVITY
-        mpu_accel_z = mpu_values['AcZ'] / ACCEL_SENSITIVITY
+        mpu_accel_x = mpu_values['AcX'] / Sensitivities.ACCEL
+        mpu_accel_y = mpu_values['AcY'] / Sensitivities.ACCEL
+        mpu_accel_z = mpu_values['AcZ'] / Sensitivities.ACCEL
 
-        mpu_gyro_x = mpu_values['GyX'] / GYRO_SENSITIVITY
-        mpu_gyro_y = mpu_values['GyY'] / GYRO_SENSITIVITY
-        mpu_gyro_z = mpu_values['GyZ'] / GYRO_SENSITIVITY
+        mpu_gyro_x = mpu_values['GyX'] / Sensitivities.GYRO
+        mpu_gyro_y = mpu_values['GyY'] / Sensitivities.GYRO
+        mpu_gyro_z = mpu_values['GyZ'] / Sensitivities.GYRO
 
         mpu_temperature = mpu_values['Tmp']
 
@@ -61,40 +52,40 @@ while True:
 
         is_acceleration_critical = False
 
-        if accel_total > ACCEL_CRITICAL_THRESHOLD or \
-        accel_max > ACCEL_SINGLE_THRESHOLD:
+        if accel_total > Thresholds.ACCEL_CRITICAL or \
+        accel_max > Thresholds.ACCEL_CRITICAL_SINGLE:
             oled.text('POSSIVEL IMPACTO', 0, oled_line_index)
             oled_line_index += 15
             oled.text('OU QUEDA', 0, oled_line_index)
             oled_line_index += 30
 
-            led_percent_red, led_percent_green, led_percent_blue = 100, 0, 0
-            set_led_color(led_percent_red, led_percent_green, led_percent_blue)
+            led_red_percent, led_green_percent, led_blue_percent = 100, 0, 0
+            set_led_color(led_red_percent, led_green_percent, led_blue_percent)
 
             is_acceleration_critical = True
-        elif accel_total > ACCEL_DANGER_THRESHOLD or \
-        accel_max > ACCEL_SINGLE_DANGER_THRESHOLD:
+        elif accel_total > Thresholds.ACCEL_DANGER or \
+        accel_max > Thresholds.ACCEL_DANGER_SINGLE:
             oled.text('ACELERACAO', 0, oled_line_index)
             oled_line_index += 15
             oled.text('PERIGOSA', 0, oled_line_index)
             oled_line_index += 30
 
-            led_percent_red, led_percent_green, led_percent_blue = 100, 100, 0
-            set_led_color(led_percent_red, led_percent_green, led_percent_blue)
+            led_red_percent, led_green_percent, led_blue_percent = 100, 100, 0
+            set_led_color(led_red_percent, led_green_percent, led_blue_percent)
 
-        if gyro_total > GYRO_DANGER_THRESHOLD:
+        if gyro_total > Thresholds.GYRO_DANGER:
             oled.text('GIRO PERIGOSO', 0, oled_line_index)
             oled_line_index += 15
             
             if not is_acceleration_critical:
-                led_percent_red, led_percent_green, led_percent_blue = 100, 100, 0
-                set_led_color(led_percent_red, led_percent_green, led_percent_blue)
+                led_red_percent, led_green_percent, led_blue_percent = 100, 100, 0
+                set_led_color(led_red_percent, led_green_percent, led_blue_percent)
             
         if oled_line_index == 5:
             oled.text('STATUS: OK', 0, oled_line_index)
 
-            led_percent_red, led_percent_green, led_percent_blue = 0, 100, 0
-            set_led_color(led_percent_red, led_percent_green, led_percent_blue)
+            led_red_percent, led_green_percent, led_blue_percent = 0, 100, 0
+            set_led_color(led_red_percent, led_green_percent, led_blue_percent)
 
     except OSError as error:
         print('Erro ao ler o sensor: ', error)
